dd549d2fda304fe6bb4282dbaabc6e8fbde65acf
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-03 11:44:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/gabrieloliveira/course/resources/UserResources.java
@@ -0,0 +1,34 @@
+package com.gabrieloliveira.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrieloliveira.course.entities.User;
+import com.gabrieloliveira.course.service.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+
+}
