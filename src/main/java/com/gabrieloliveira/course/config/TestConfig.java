ef20001a5a378e1ce261c8e2749fac9111f524e3
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-03 22:10:33 -0300

    Enums

--- src/main/java/com/gabrieloliveira/course/config/TestConfig.java
+++ src/main/java/com/gabrieloliveira/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import com.gabrieloliveira.course.entities.Category;
 import com.gabrieloliveira.course.entities.Order;
 import com.gabrieloliveira.course.entities.Product;
 import com.gabrieloliveira.course.entities.User;
+import com.gabrieloliveira.course.entities.enums.OrderStatus;
 import com.gabrieloliveira.course.repositories.CategoryRepository;
 import com.gabrieloliveira.course.repositories.OrderRepository;
 import com.gabrieloliveira.course.repositories.ProductRepository;
@@ -40,9 +41,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
@@ -59,8 +60,8 @@ public class TestConfig implements CommandLineRunner {
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 	}
 
--- src/main/java/com/gabrieloliveira/course/entities/Order.java
+++ src/main/java/com/gabrieloliveira/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gabrieloliveira.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,16 +27,21 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+
+	
+	
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -62,6 +68,24 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
+	}
+
+	/*public Double total() {
+		Double total = 0.0;
+		for (OrderItem oi : orders) {
+			total += oi.subtotal();
+		}
+		return total;
+	}*/
+	
 
 	@Override
 	public int hashCode() {
