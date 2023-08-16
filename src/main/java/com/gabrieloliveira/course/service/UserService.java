990f3dad6963e2c158239657efe3d3847799f9de
Gabriel Oliveira <gabriel.dev04@gmail.com>
2023-08-16 09:18:18 -0300

    Exception handling - findById

--- src/main/java/com/gabrieloliveira/course/service/UserService.java
+++ src/main/java/com/gabrieloliveira/course/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabrieloliveira.course.entities.User;
 import com.gabrieloliveira.course.repositories.UserRepository;
+import com.gabrieloliveira.course.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/gabrieloliveira/course/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.gabrieloliveira.course.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not Found. Id: " + id);
+	}
+}
